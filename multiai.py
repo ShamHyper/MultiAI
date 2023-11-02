bb683a4b0266201b4e2561361e86a06eb327add7
BuildTools <unconfigured@null.spigotmc.org>
2023-11-02 06:13:34 +0300

    1.6.13 | HF+CV2 render

--- multiai.py
+++ multiai.py
@@ -17,13 +17,10 @@ from clip_interrogator import Config, Interrogator
 from transformers import GPT2Tokenizer, GPT2LMHeadModel, pipeline
 
 import cv2
-import torch
-import torchvision
-from torchvision import transforms
-from torchvision.transforms.functional import to_pil_image
+from numba import cuda
 
 class init:
-    ver = "MultiAI v1.6.12"
+    ver = "MultiAI v1.6.13"
     print(f"Initializing {ver} launch...")
     
     with open("config.json") as json_file:
@@ -335,13 +332,14 @@ class multi:
         
         return bth_Vspc_output
     
-    def process_frame_legacy(frame):
+    def process_frame(frame):
         result_frame = frame    
         return result_frame
-  
+
     def bth_Vspc(video_dir, vbth_slider, threshold_Vspc_slider):
         nsfw_load()
-        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        _nsfw = 0
+        _plain = 0
         output_dir = 'tmp_pngs'
         i1 = 0
         os.makedirs(output_dir, exist_ok=True)
@@ -350,21 +348,25 @@ class multi:
         for dir_Vspc in tqdm(video_files):
             i1 += 1
             print(f"[{i1}]Predicting frames...")
-
-            video = torchvision.io.read_video(os.path.join(video_dir, dir_Vspc))[0].to(device)
-            num_frames, _, height, width = video.shape
-
+            cap = cv2.VideoCapture(os.path.join(video_dir, dir_Vspc))
             frame_count = 0
-            
-            for frame_count in range(num_frames):
-                frame_gpu = video[frame_count].permute(1, 2, 0).unsqueeze(0).to(device)
-                frame_gpu_byte = (frame_gpu * 255).clamp(0, 255).byte() 
-                frame_gpu_byte = frame_gpu_byte[:, :3, :, :]
-                frame_pil = to_pil_image(frame_gpu_byte.squeeze().cpu())
+
+            while cap.isOpened():
+                ret, frame = cap.read()
+                if not ret:
+                    break
+
+                frame_gpu = cuda.to_device(frame)
+                processed_frame_gpu = multi.process_frame(frame_gpu)
+
+                processed_frame = processed_frame_gpu.copy_to_host()
+
                 output_file = os.path.join(output_dir, f'{frame_count + 1}.png')
-                frame_pil.save(output_file)
+                cv2.imwrite(output_file, processed_frame)
 
                 frame_count += 1
+
+            cap.release()
             
             total_sum = 0
             file_count = 0
@@ -394,14 +396,25 @@ class multi:
             if (value_nsfw_1 > THRESHOLD or value_nsfw_2 > THRESHOLD or value_nsfw_3 > THRESHOLD * 1.5) and value_sfw < THRESHOLD:
                 video_path = os.path.join(video_dir, dir_Vspc)
                 sh.copy(video_path, 'video_analyze_nsfw')
+                _nsfw += 1
             else:
                 video_path = os.path.join(video_dir, dir_Vspc)
                 sh.copy(video_path, 'video_analyze_plain')
+                _plain += 1
+                
+            cap.release()
+            cv2.destroyAllWindows()
             
             rm_tmp = os.path.join(init.current_directory, output_dir)
             sh.rmtree(rm_tmp)
             os.makedirs(output_dir, exist_ok=True)
-            
+              
+            out_cmd = percentages
+            out_cmd += f"\nNSFW: {_nsfw}"
+            out_cmd += f"\nPlain: {_plain}"
+            print(out_cmd)
+            print("")
+            out_cmd = None
             avg_sum = None
             percentages = None
         bth_Vspc_output = "Ready!"
